e5c033c5c33c4edb7254527d1fea294ac77f46d1
Rodrigo Rosa <rodsiqueirabh@hotmail.com>
2026-08-18 01:23:14 -0300

    feat: Incluindo variável de ambiente e testando no HTML e TSX

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  console.log("VARIAVEL VITE_API_URL:", import.meta.env.VITE_API_URL);
 
   return (
     <>
@@ -116,7 +118,7 @@ function App() {
       <div className="ticks"></div>
       <section id="spacer"></section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
